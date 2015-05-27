726c90bcdb7db8b180c823b0d7ee5b9fec8ef862
FlyElephant <1061731917@qq.com>
2015-05-27 10:01:56 +0800

    Initial Commit

---
+++ AlogrithmAll/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AlogrithmAll
+//
+//  Created by keso on 15/5/27.
+//  Copyright (c) 2015年 keso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
